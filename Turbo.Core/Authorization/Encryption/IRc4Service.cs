da24ed8c9a82a6472f7d3fc5d118eae74b30bb6e
Bill <billsonnn@billsonnn.com>
2025-08-26 21:05:53 -0400

    Remove files

--- Turbo.Core/Authorization/Encryption/IRc4Service.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Turbo.Core.Authorization.Encryption;
-
-public interface IRc4Service
-{
-    void SetKey(byte[] key);
-    byte[] Encrypt(byte[] inputData);
-    byte[] Decrypt(byte[] inputData);
-}
