63ac75678193abf66456b6aeb71b41997e76b2f2
billsonnn <bill@billsonnn.com>
2025-11-09 16:16:18 -0500

    Updates

--- start-turbo.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
-$env:DOTNET_ENVIRONMENT = "Production"
-Push-Location $PSScriptRoot
-dotnet "$PSScriptRoot/Turbo.Main.dll" @args
-Pop-Location
