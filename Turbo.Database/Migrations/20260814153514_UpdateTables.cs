1e4090bfd14fe48e1946189eede14c2ed58085cd
billsonnn <bill@billsonnn.com>
2026-08-14 11:52:27 -0400

    Updates

--- Turbo.Database/Migrations/20260814153514_UpdateTables.cs
+++ Turbo.Database/Migrations/20260814153514_UpdateTables.cs
@@ -10,37 +10,43 @@ namespace Turbo.Database.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AlterColumn<string>(
-                name: "paint_wall",
-                table: "rooms",
-                type: "varchar(512)",
-                maxLength: 512,
-                nullable: true,
-                oldClrType: typeof(double),
-                oldType: "double",
-                oldDefaultValue: 0.0)
+            migrationBuilder
+                .AlterColumn<string>(
+                    name: "paint_wall",
+                    table: "rooms",
+                    type: "varchar(512)",
+                    maxLength: 512,
+                    nullable: true,
+                    oldClrType: typeof(double),
+                    oldType: "double",
+                    oldDefaultValue: 0.0
+                )
                 .Annotation("MySql:CharSet", "utf8mb4");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "paint_landscape",
-                table: "rooms",
-                type: "varchar(512)",
-                maxLength: 512,
-                nullable: true,
-                oldClrType: typeof(double),
-                oldType: "double",
-                oldDefaultValue: 0.0)
+            migrationBuilder
+                .AlterColumn<string>(
+                    name: "paint_landscape",
+                    table: "rooms",
+                    type: "varchar(512)",
+                    maxLength: 512,
+                    nullable: true,
+                    oldClrType: typeof(double),
+                    oldType: "double",
+                    oldDefaultValue: 0.0
+                )
                 .Annotation("MySql:CharSet", "utf8mb4");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "paint_floor",
-                table: "rooms",
-                type: "varchar(512)",
-                maxLength: 512,
-                nullable: true,
-                oldClrType: typeof(double),
-                oldType: "double",
-                oldDefaultValue: 0.0)
+            migrationBuilder
+                .AlterColumn<string>(
+                    name: "paint_floor",
+                    table: "rooms",
+                    type: "varchar(512)",
+                    maxLength: 512,
+                    nullable: true,
+                    oldClrType: typeof(double),
+                    oldType: "double",
+                    oldDefaultValue: 0.0
+                )
                 .Annotation("MySql:CharSet", "utf8mb4");
 
             migrationBuilder.AlterColumn<short>(
@@ -51,46 +57,53 @@ namespace Turbo.Database.Migrations
                 defaultValue: (short)0,
                 oldClrType: typeof(int),
                 oldType: "int",
-                oldDefaultValue: 0);
+                oldDefaultValue: 0
+            );
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AlterColumn<double>(
-                name: "paint_wall",
-                table: "rooms",
-                type: "double",
-                nullable: false,
-                defaultValue: 0.0,
-                oldClrType: typeof(string),
-                oldType: "varchar(512)",
-                oldMaxLength: 512,
-                oldNullable: true)
+            migrationBuilder
+                .AlterColumn<double>(
+                    name: "paint_wall",
+                    table: "rooms",
+                    type: "double",
+                    nullable: false,
+                    defaultValue: 0.0,
+                    oldClrType: typeof(string),
+                    oldType: "varchar(512)",
+                    oldMaxLength: 512,
+                    oldNullable: true
+                )
                 .OldAnnotation("MySql:CharSet", "utf8mb4");
 
-            migrationBuilder.AlterColumn<double>(
-                name: "paint_landscape",
-                table: "rooms",
-                type: "double",
-                nullable: false,
-                defaultValue: 0.0,
-                oldClrType: typeof(string),
-                oldType: "varchar(512)",
-                oldMaxLength: 512,
-                oldNullable: true)
+            migrationBuilder
+                .AlterColumn<double>(
+                    name: "paint_landscape",
+                    table: "rooms",
+                    type: "double",
+                    nullable: false,
+                    defaultValue: 0.0,
+                    oldClrType: typeof(string),
+                    oldType: "varchar(512)",
+                    oldMaxLength: 512,
+                    oldNullable: true
+                )
                 .OldAnnotation("MySql:CharSet", "utf8mb4");
 
-            migrationBuilder.AlterColumn<double>(
-                name: "paint_floor",
-                table: "rooms",
-                type: "double",
-                nullable: false,
-                defaultValue: 0.0,
-                oldClrType: typeof(string),
-                oldType: "varchar(512)",
-                oldMaxLength: 512,
-                oldNullable: true)
+            migrationBuilder
+                .AlterColumn<double>(
+                    name: "paint_floor",
+                    table: "rooms",
+                    type: "double",
+                    nullable: false,
+                    defaultValue: 0.0,
+                    oldClrType: typeof(string),
+                    oldType: "varchar(512)",
+                    oldMaxLength: 512,
+                    oldNullable: true
+                )
                 .OldAnnotation("MySql:CharSet", "utf8mb4");
 
             migrationBuilder.AlterColumn<int>(
@@ -101,7 +114,8 @@ namespace Turbo.Database.Migrations
                 defaultValue: 0,
                 oldClrType: typeof(short),
                 oldType: "smallint",
-                oldDefaultValue: (short)0);
+                oldDefaultValue: (short)0
+            );
         }
     }
 }
